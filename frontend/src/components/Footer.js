a3564a9624748133e29995c6d55cd5aec9d7747c
Patric Kangasmäki <patric.kangasmaki@tuni.fi>
2021-02-04 11:00:56 +0200

    Footer for the page

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style={{marginTop: 30}}> 
+      <i> Workout app for <a href="https://fullstackopen.com/"> fullstack open </a>by pkangasmaki -<a href="https://github.com/pkangasmaki/"> GitHub </a> </i>
+    </div>
+  )
+}
+
+export default Footer
