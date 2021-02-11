6898b4f6c648b4cfb942ba3e15a3d99b734dbd48
Patric Kangasmäki <patric.kangasmaki@tuni.fi>
2021-02-11 12:19:17 +0200

    Styling for suggestions page

--- frontend/src/components/Suggestions.js
+++ frontend/src/components/Suggestions.js
@@ -1,32 +1,54 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
 
 const Suggestions = () => {
   return (
     <div>
       <h3>Suggested routines for beginner/intermediate athletes</h3>
-      <p>Push pull legs: 
-        <i>
-          <a href="https://medium.com/@BradNewtonTV/the-ultimate-beginner-guide-push-pull-legs-5d576162868f"> https://medium.com/@BradNewtonTV/the-ultimate-beginner-guide-push-pull-legs-5d576162868f</a>
-        </i>
-      </p>
+      <Card style={{ width: '18rem', float: 'left' }}>
+        <Card.Body>
+          <Card.Title>PPL</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Push pull legs</Card.Subtitle>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Card.Link href="https://medium.com/@BradNewtonTV/the-ultimate-beginner-guide-push-pull-legs-5d576162868f">PPL routine @Medium.com</Card.Link>
+        </Card.Body>
+      </Card>
 
-      <p>full body: 
-        <i>
-          <a href="https://builtwithscience.com/best-full-body-workout/"> https://builtwithscience.com/best-full-body-workout/</a>
-        </i>
-      </p>
+      <Card style={{ width: '18rem', float: 'left' }}>
+        <Card.Body>
+          <Card.Title>Full body</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Card.Link href="https://builtwithscience.com/best-full-body-workout/">Full body routine @Buildwithscience.com</Card.Link>
+        </Card.Body>
+      </Card>
 
-      <p>upper/lower body: 
-        <i>
-          <a href="https://www.muscleandstrength.com/workouts/upper-lower-4-day-gym-bodybuilding-workout"> https://https://www.muscleandstrength.com/workouts/upper-lower-4-day-gym-bodybuilding-workout</a>
-        </i>
-      </p>
+      <Card style={{ width: '18rem', float: 'left' }}>
+        <Card.Body>
+          <Card.Title>Upper/Lower body</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Card.Link href="https://www.muscleandstrength.com/workouts/upper-lower-4-day-gym-bodybuilding-workout">Upper/lower routine @muscleandstrength.com</Card.Link>
+        </Card.Body>
+      </Card>
 
-      <p>bro split: 
-        <i>
-          <a href="https://victoremgear.com/blogs/resistance-training/5-day-workout-split"> https://victoremgear.com/blogs/resistance-training/5-day-workout-split</a>
-        </i>
-      </p>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>5-day split</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.
+          </Card.Text>
+          <Card.Link href="https://victoremgear.com/blogs/resistance-training/5-day-workout-split">5-day split routine @Victoremgear.com</Card.Link>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
